10aee55c31542885a996cd9f7752031dae0b10fd
Luuk <Luuk@DESKTOP-TSB3G0E>
2017-06-27 19:11:05 +0200

    Add project files.

---
+++ DTA1-RecommendationSystem2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTA1_RecommendationSystem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
